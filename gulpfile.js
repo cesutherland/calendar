11dbb9a1363d10b42e222c8ec3a8aff292a3a9f3
Carl Sutherland <carl@humblesoftware.com>
2016-07-09 08:39:17 -0700

    Add Bootstrap.
    Yiiich.

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,30 @@ var gulp              = require('gulp');
 var webpackRecipe     = require('gib-recipe-webpack');
 
 
+// Config: 
+
+var options = {
+  entry: {
+    app: './src/index.js',
+    css: '!style!css!less!bootstrap/less/bootstrap.less'
+  },
+  dest: path.resolve(__dirname, 'build'),
+  loaders: [
+    { test: /\.less$/, loader: 'style!css!less' },
+    { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url?limit=100000' }
+  ]
+};
+
+
 // Tasks:
 
 gulp.task('default', ['webpack', 'webpack-server']);
 
-
 gulp.task('webpack', function () {
   return gulp.src('src/index.js')
-    .pipe(webpackRecipe.webpack({
-      dest: path.resolve(__dirname, 'build')
-    }))
+    .pipe(webpackRecipe.webpack(options))
     .on('error', console.error)
     .pipe(gulp.dest('build/'));
 });
 
-
-gulp.task('webpack-server', webpackRecipe.server({
-  dest: path.resolve(__dirname, 'build'),
-}));
+gulp.task('webpack-server', webpackRecipe.server(options));
